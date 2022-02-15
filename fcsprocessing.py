f980892adb1509e9de15b10814f9864e06b86a45
Sneha Jaikumar <snehaj@email.unc.edu>
2022-02-15 11:34:28 -0500

    Combined raw fcs files

--- fcsprocessing.py
+++ fcsprocessing.py
@@ -1,33 +1,48 @@
 import numpy as np
 import flowkit as fk
 import pandas as pd
+import ntpath
 
-fcs_path =  'FlowRepository_FR-FCM-ZYJP_files/BC12_all_cells_raw.fcs'
+#TO-DO
+#simplify to 10000 cells per fcs file/patient (only raw)
+#keep track of which file each cell came from-for loop or parse
 
-sample = fk.Sample(fcs_path)
 
 
-print(sample)
+#function to create matrix
+def create_matrix(filePath):
+    sample = fk.Sample(filePath)
+    #print("Information about Sample: ", sample)
+    dm = sample._get_raw_events()
+    CNames = np.array(sample.pns_labels)
+    #select markers of interest
+    #toKeep = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49]
+    toKeep = [2,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,49]
+    dm_trans= np.arcsinh(1./5 * dm[:,toKeep])
+    CNames = CNames[toKeep]
+    df_nice = pd.DataFrame(dm_trans,columns=CNames)
+    df_nice = df_nice.head(10)
+    df_nice.insert(0,"File Name", [ntpath.basename(filePath) for i in range(10)], True)
+    #df_nice.insert(0, "Index", [i for i in range(10)], True)
+    return df_nice
+    #df_columns = pd.DataFrame(df_nice.columns)
+    #print("Columns Info: ", df_columns)
 
-dm = sample._get_raw_events()
+#merge all raw files
+end = pd.concat([create_matrix('FlowRepository_FR-FCM-ZYJP_files/BC12_all_cells_raw.fcs'), create_matrix('FlowRepository_FR-FCM-ZYJP_files/BC13_all_cells_raw.fcs')], sort = False)
+end = pd.concat([end,create_matrix('FlowRepository_FR-FCM-ZYJP_files/BC14_all_cells_raw.fcs')],sort = False)
+print(end)
 
-CNames = np.array(sample.pns_labels)
+#first raw file
+#print(create_matrix('FlowRepository_FR-FCM-ZYJP_files/BC12_all_cells_raw.fcs'))
+#second raw file
+#print(create_matrix('FlowRepository_FR-FCM-ZYJP_files/BC13_all_cells_raw.fcs'))
 
-toKeep = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49]
+# #third raw file
+# create_matrix('FlowRepository_FR-FCM-ZYJP_files/BC14_all_cells_raw.fcs')
 
-##################################################################
-#yfinally create your data matrix for further analysis
-##################################################################
 
-#now we will transform the data and select our columns/markers of interest
-dm_trans= np.arcsinh(1./5 * dm)
-CNames = CNames[toKeep]
 
-#you could have stopped there, but we can make a 'nice' dataframe
-df_nice = pd.DataFrame(dm_trans,columns=CNames)
-print(df_nice)
 
-df_columns = pd.DataFrame(df_nice.columns)
 
-print(df_columns)
 
